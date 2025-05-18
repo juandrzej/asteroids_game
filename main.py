52031d1d6de3a9077f1823d034f9f6d06e3d2744
juandrzej <programming.yippee729@passinbox.com>
2025-05-18 18:42:10 +0200

    groups prepared

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main() -> None:
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Starting game, clock, dt, screen and display
     pygame.init()
     clock: pygame.time.Clock = pygame.time.Clock()
     dt: float = 0.0
@@ -14,6 +16,11 @@ def main() -> None:
         (SCREEN_WIDTH, SCREEN_HEIGHT)
     )
     pygame.display.init()
+
+    # Creating updatable and drawable groups, starting player instance
+    updatable: pygame.sprite.Group = pygame.sprite.Group()
+    drawable: pygame.sprite.Group = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -23,8 +30,9 @@ def main() -> None:
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
         # Basically screen update
         pygame.display.flip()
