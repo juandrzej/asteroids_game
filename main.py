a017303105866347a1bdb83dfbb70359f8c3b6d0
juandrzej <programming.yippee729@passinbox.com>
2025-05-18 15:19:27 +0200

    game initialization working, paint black, exit works

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main() -> None:
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    print(pygame.get_init())
+    clock = pygame.time.Clock()
+    dt: float = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.init()
 
@@ -17,6 +18,8 @@ def main() -> None:
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
